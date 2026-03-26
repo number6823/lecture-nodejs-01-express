3c7d448d0fa08c4041e5cab21ceba2c7a9b762a4
number6823 <ohdehet@naver.com>
2026-03-26 12:15:41 +0900

    환경변수 적용

--- src/index.ts
+++ src/index.ts
@@ -41,8 +41,8 @@ app.get("/posts",(req,res) => {
 
 // 3. app.listen : 서버를 실행하는 메소드
 // 매개변수 2개 (포트번호,함수)
-app.listen(3000,() => {
+app.listen(process.env.PORT,() => {
      // 얘가 실행되면 처음 할 일
-    console.log("서버가 실행되었습니다. http://localhost:3000");
+    console.log(`서버가 실행되었습니다. http://localhost:${process.env.PORT}`);
 });
 
