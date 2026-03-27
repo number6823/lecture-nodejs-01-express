69dcccbcda841a0a2d4307428bc348b7cdcb17a6
number6823 <ohdehet@naver.com>
2026-03-27 12:41:16 +0900

    Router 분리

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import postRouter from "./routes/postRouter.js";
 // 1. 환경 변수 초기화
 dotenv.config();
 
 // 2. Express 앱 생성
 const app = express();
-app.use(express.json());        // app.use() 메소드는 미들웨어를 사용하게  할 때 사용
-
-
-// 데이터베이스가 없어서 임시로 만든 데이터 변수  (mock)
-const mockPosts = [
-    { id: 1, title: "첫 번째 택배", content: "무사히 도착했습니다." },
-    { id: 2, title: "두 번째 택배", content: "파손 주의해주세요!" },
-    { id: 3, title: "세 번째 택배", content: "문 앞에 두고 가주세요." },
-];
-
+app.use(express.json()); // app.use() 메소드는 미들웨어를 사용하게  할 때 사용
 
 // "/" 로 들어왔을 때 동작되는 일꾼
- app.get("/",(req,res) => {
-     // res.send() 메소드는 string을 내보낼 때 사용
-     res.send ("여기는 루트입니다.");
-})
+app.get("/", (req, res) => {
+    // res.send() 메소드는 string을 내보낼 때 사용
+    res.send("여기는 루트입니다.");
+});
 
-app.get("/hello",(a,b) => {
-    b.send("여기는 hello 주소로 들어왔습니다.")
-})
+app.get("/hello", (a, b) => {
+    b.send("여기는 hello 주소로 들어왔습니다.");
+});
 
-app.get("/posts",(req,res) => {
-    // res.json(보낼데이터) 메소드 : string이 아닌, 객체 타입의 데이터 (단, 함수 빼고) 를 보낼 때 사용
-    // les.json 메소드를 사용하려면 app.use(express.json()) 을 꼭 써줘야 함
-    res.json(mockPosts);
-})
+app.use(postRouter);
 
 //app.get("/",(req,res)=> {
-  //  res.send("<h1>Express 서버</h1><div>이렇게 쓰는건 너무 힘드네요</div>");
+//  res.send("<h1>Express 서버</h1><div>이렇게 쓰는건 너무 힘드네요</div>");
 // })
-
-
 
 // 3. app.listen : 서버를 실행하는 메소드
 // 매개변수 2개 (포트번호,함수)
-app.listen(process.env.PORT,() => {
-     // 얘가 실행되면 처음 할 일
+app.listen(process.env.PORT, () => {
+    // 얘가 실행되면 처음 할 일
     console.log(`서버가 실행되었습니다. http://localhost:${process.env.PORT}`);
 });
-
